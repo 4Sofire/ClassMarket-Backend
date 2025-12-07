43ee3778d09e3c207681a837fa7ced421efbba17
4Sofire <137798321+4Sofire@users.noreply.github.com>
2025-12-07 22:33:26 +0400

    Middlewares

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://ob384:AewneOTcxz3dx1yC@after-mdx.w9ple.mongodb.net/';
 const DB_NAME = 'aftermdx';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+  console.log(`A ${req.method} request come from ${req.url}`);
+  next();
+});
