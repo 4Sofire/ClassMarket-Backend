ca15751bda7aec570bb757d00efc2fa47ba4ed2b
4Sofire <137798321+4Sofire@users.noreply.github.com>
2025-12-07 22:22:34 +0400

    Mongo Demo

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ObjectId } = require('mongodb');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://ob384:AewneOTcxz3dx1yC@after-mdx.w9ple.mongodb.net/';
+const DB_NAME = 'aftermdx';
